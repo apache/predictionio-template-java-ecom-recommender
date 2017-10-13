358d83f285e0c2c73fc3dbc1e945051c1bea530f
shimamoto <shimamoto@apache.org>
2017-10-13 17:01:47 +0900

    0.12.0-incubating release

--- data/send_query.py
+++ data/send_query.py
@@ -4,4 +4,4 @@ Send sample query to prediction engine
 
 import predictionio
 engine_client = predictionio.EngineClient(url="http://localhost:8000")
-print engine_client.send_query({"userEntityId": "u1", "number": 4})
+print(engine_client.send_query({"userEntityId": "u1", "number": 4}))
